84d1b59dd9eba1ef11bff27990aa1b8cf0e9a939
saiemsaeed <33630921+saiemsaeed@users.noreply.github.com>
2018-03-28 00:56:40 +0500

    Port Added

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.post('/add', (req, res) => {
 });
 
 
-const port = porcess.env.port || 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`Server is listening on PORT ${port}!!!`);
 });
