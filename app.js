566da85ee8cc464dfb1568d8f09be1699cc90f8a
saiemsaeed <33630921+saiemsaeed@users.noreply.github.com>
2018-03-28 00:23:23 +0500

    Many Features Added!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 const bodyParser = require("body-parser");
 
 let app = express();
@@ -11,17 +11,84 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/addUser', (req, res) => {
+app.get('/id/:id', (req, res) => {
+
+    var _id = new ObjectID(req.params.id);
+    console.log(_id);
+    MongoClient.connect('mongodb://localhost:27017/EventApp', (err, client) => {
+        if(err){
+            console.log(err);
+        }
+        const db = client.db('EventApp');
+        db.collection('registeredStudents').findOne({_id})
+        .then((data) => {
+            console.log(data);
+            res.render('index', {
+                data
+            });
+        });
+    });
+});
+
+
+app.get('/find/:id', (req, res) => {
+    var id = req.params.id.toLowerCase();
+    MongoClient.connect('mongodb://localhost:27017/EventApp', (err, client) => {
+        if(err){
+            console.log(err);
+        }
+        const db = client.db('EventApp');
+        db.collection('registeredStudents').find({"reg" : {$regex: `.*${id}.*`}}).toArray()
+        .then((data) => {
+            res.send(data);
+        });
+    });
+});
+
+app.get('/find/', (req, res) => {
+    MongoClient.connect('mongodb://localhost:27017/EventApp', (err, client) => {
+        if(err){
+            console.log(err);
+        }
+        const db = client.db('EventApp');
+        db.collection('registeredStudents').find({}).toArray()
+        .then((data) => {
+            res.send(data);
+        });
+    });
+})
+
+app.get('/users', (req, res) => {
+    MongoClient.connect('mongodb://localhost:27017/EventApp', (err, client) => {
+        if(err){
+            return console.log('Unable to connect to Database', err);
+        }
+        
+        const db = client.db('EventApp');
+        db.collection('registeredStudents').find({}).toArray()
+        .then((data) => {
+            res.render('registered', {data, message: "Enter Registration Id To Find"});
+        });
+    });
+});
+
+app.post('/add', (req, res) => {
     MongoClient.connect('mongodb://localhost:27017/EventApp', (err, client) => {
         if(err){
             return console.log(`Unable to connect to Database`, err);
         }
+        var name = req.body.name;
+        var reg = req.body.reg.toLowerCase();
+        var email = req.body.email || "";
+        var phone = req.body.phone || "";
         console.log("Connected to Database!");
         const db = client.db('EventApp');
 
         db.collection('registeredStudents').insertOne({
-            name: req.body.name,
-            reg: req.body.reg,
+            name,
+            reg,
+            email,
+            phone
         })
         .then((result) => {
             console.log(result.ops);
