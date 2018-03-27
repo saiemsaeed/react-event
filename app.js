adf849f71f82f9b8482b04152b6d496088cd8444
saiemsaeed <33630921+saiemsaeed@users.noreply.github.com>
2018-03-28 01:02:01 +0500

    Problem Solved

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.post('/add', (req, res) => {
 });
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is listening on PORT ${port}!!!`);
 });
